67cf87b9954b60088a8b6c2bf58cdb2d35f6e9a7
balasaranirali <balasaranirali23@gmail.com>
2026-07-02 10:53:00 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,29 @@
+<?php
+$a=array(1,2,3);
+echo $a[0];
+echo $a[1];
+echo $a[2];
+
+$b=array(1.1,1.2,1.3);
+echo $b[0];
+echo $b[1];
+echo $b[2];
+
+
+$c=array("jiyu","niru","piyu","banu");
+echo $c[0];
+echo $c[1];
+echo $c[2];
+
+$x=array(10,"niru",30.00);
+echo $x[0];
+echo $x[1];
+echo $x[2];
+
+print_r($x);
+echo"<br>";
+var_dump($x);
+
+
+
+?>
